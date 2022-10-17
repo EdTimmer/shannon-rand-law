f35586c5dac3bf6e6fb800288a9ff63dd475450a
Ed Timmer <edtimmer@gmail.com>
2022-10-17 11:16:20 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@
 import { AppStyles } from './App.css.js';
 import Home from './pages/Home';
 import About from './pages/About';
-import HeadRow from './components/HeadRow';
-// import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import ServicesComponent from './pages/ServicesComponent';
 import Contact from './pages/Contact';
 
 const App = () => (
   <AppStyles>
-    {/* <HeadRow /> */}
-    {/* <Navigation /> */}
     <Home />
-    {/* <HashRouter>
-      <Switch>
-        <Route exact path="/about" component={About} />
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </HashRouter> */}
     <div id="about">
       <About />
     </div>
